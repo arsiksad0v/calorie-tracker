47a7cb3929292459594c205d559c94ec8c000900
Arstan <arsiksad0v.wgod@gmail.com>
2024-07-08 22:08:50 +0600

    Added routing structure with react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import App from './App';
+import Home from './pages/Home';
+import MealEdit from './pages/MealEdit';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="/meals/new" element={<MealEdit />} />
+          <Route path="/meals/:id/edit" element={<MealEdit />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
