87958fe9e1f10ba67cf8cc103a7a6cef4525f30d
Arstan <arsiksad0v.wgod@gmail.com>
2024-07-09 16:42:22 +0600

    Bug fixes

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 const rootElement = document.getElementById('root');
 
 if (rootElement) {
-  const root = ReactDOM.createRoot(rootElement);
+  const root = createRoot(rootElement);
   root.render(
     <React.StrictMode>
       <BrowserRouter>
@@ -14,9 +14,4 @@ if (rootElement) {
       </BrowserRouter>
     </React.StrictMode>
   );
-} else {
-  console.error('Root element not found');
 }
-
-
-  
